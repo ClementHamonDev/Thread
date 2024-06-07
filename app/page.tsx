e40db427b8dc1cf5fdb7658c21a809f786a03c6c
ClementHamonDev <clement.hamon35@gmail.com>
2024-06-07 20:17:09 +0200

    ajout auth git

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+'use server';
+
 import Post from "@/src/features/card/Post";
 import { ScrollArea } from "@/components/ui/scroll-area"
+import { getAuthSession } from "@/lib/auth";
 
-
-export default function Home() {
+export default async function Home() {
+  let session = await getAuthSession()
   return (
     <div className="px-4 m-w-lg">
       <ScrollArea>
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
 import GithubProvider from "next-auth/providers/github"
 import { env } from "./env"
 import { prisma } from "./prisma"
-import { AuthOptions } from "next-auth"
+import { AuthOptions, getServerSession } from "next-auth"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 
 export const authOptions: AuthOptions = {
@@ -10,6 +10,11 @@ export const authOptions: AuthOptions = {
     GithubProvider({
       clientId: env.GITHUB_ID,
       clientSecret: env.GITHUB_SECRET,
-    }),
-  ],
+    })
+  ]
+}
+
+export const getAuthSession = async() => {
+  const session = await getServerSession(authOptions);
+  return session;
 }
